225f13e2a370754cd55373a65fba7d1bf8bfbdf8
Saba Gotsiridze <gotsiridze.s@outlook.com>
2023-12-07 16:33:12 +0400

    ValueObjects

---
+++ BuberDinner.Domain/Common/ValueObjects/AverageRating.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuberDinner.Domain.Common.Models;
+
+namespace BuberDinner.Domain.Common.ValueObjects;
+
+public sealed class AverageRating : ValueObject
+{
+    public double Value { get; private set; }
+    public int NumberOfRatings { get; private set; }
+
+    private AverageRating(double value, int numberOfRatings)
+    {
+        Value = value;
+        NumberOfRatings = numberOfRatings;
+    }
+
+    public static AverageRating CreateNew(double rating = 0, int numberOfRatings = 0)
+    {
+        return new AverageRating(rating, numberOfRatings);
+    }
+
+    public void AddNewRating(Rating rating)
+    {
+        Value = ((Value * NumberOfRatings) + rating.Value) / ++NumberOfRatings;
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+        yield return NumberOfRatings;
+    }
+}
